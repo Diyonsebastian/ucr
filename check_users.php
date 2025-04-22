164dff2a0efa153ec9fdcd41b14b224fa4b2c06e
Diyonsebastian <155514532+Diyonsebastian@users.noreply.github.com>
2025-04-22 10:33:03 +0530

    Update check_users.php

--- check_users.php
+++ check_users.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "root", "usedcar", 3307);
+$mysqli = new mysqli("my-mysql", "root", "root", "usedcar");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
